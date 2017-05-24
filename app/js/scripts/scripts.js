b78daa74495e3ec70942fb71ff81380e198baa83
alexrewrew <alexrewrew@MacBook-Pro-Oleksandr.local>
2017-05-24 16:10:04 +0300

    lets start

---
+++ app/js/scripts/scripts.js
@@ -0,0 +1,12 @@
+/**
+ * Created by alexrewrew on 15.02.17.
+ */
+$(document).ready(function () {
+
+    //responsive scripts
+    if (window.matchMedia('(max-width: 767px)').matches) {
+        
+    }
+})
+
+
